54f70d0772e7a2934d76ccbd0034523b4e32ae05
Julia Piascik <98356859+liapia99@users.noreply.github.com>
2023-11-08 11:51:16 -0500

    Create Class1Lab1B.py

---
+++ CS-112/Class1Lab1B.py
@@ -0,0 +1,23 @@
+"""
+File: Class1Lab1B
+Author: Julia Piascik
+Date: Jan. 17, 2023
+Selection Statements
+"""
+print("1 for Park View, 2 for Golf Course View, or 3 for Lake view")
+
+apartment = int(input("Select which view you would like to see from your apartment using the statement above."))
+
+if apartment==1:
+    print("Park view condos are $150,000.")
+elif apartment==2:
+    print("Condos with golf course views are $170,000.")
+elif apartment==3:
+    print("Lake view condos are $210,000.")
+else:
+    print("Error! Cost is $0.")
+   
+
+
+        
+    
